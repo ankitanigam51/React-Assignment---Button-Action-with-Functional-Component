c86edd48e3f1194f0cb2aa245c30a6a8a4f092fe
Ankita Nigam <ankitanigam51@gmail.com>
2020-11-17 19:20:18 +0530

    modify

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ function App() {
   const [showPara, setShowPara] = useState(false);
   return (
     <div id="main">
-      <button id="click" onClick={() => setShowPara(!showPara)}>Toggle Para</button>
+      <button id="click" onClick={() => setShowPara(true)}>Show Para</button>
       {showPara ? (<p id="para"> Hello, I've learnt to use the full-stack evaluation tool.
          This makes me so happy</p>) : null}
 </div>
