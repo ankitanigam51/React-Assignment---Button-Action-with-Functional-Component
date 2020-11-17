a7e3af5598af36257c871745a68030073f845fa0
Ankita Nigam <ankitanigam51@gmail.com>
2020-11-17 23:09:30 +0530

    Done change

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,10 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [showPara, setShowPara] = useState(false);
+  const [showPara, setShowPara] = useState(true);
   return (
     <div id="main">
-      <button id="click" onClick={() => setShowPara(true)}>Show Para</button>
+      <button id="click" onClick={() => setShowPara(!showPara)}>Show Para</button>
       {showPara ? ( <p id="para"> Hello, I've learnt to use the full-stack evaluation tool.This makes me so happy
       </p> ) : null}
 </div>
